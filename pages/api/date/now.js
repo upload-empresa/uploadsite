e19319023e66911dad8b3ff91f1b9fab2e847461
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2022-11-09 13:52:38 -0300

    vai

---
+++ pages/api/date/now.js
@@ -0,0 +1,8 @@
+export default function now(req, res) {
+  const date = new Date();
+  const format = date.toGMTString();
+
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
+  res.json({ now: format });
+}
