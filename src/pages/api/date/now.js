2db96e36706ba1d43e663a32b4de43e43ff72ec1
gabrielGit1 <botelhog275@gmail.com>
2022-11-12 17:50:14 -0300

    Adicionando otimizações

--- src/pages/api/date/now.js
+++
@@ -1,8 +0,0 @@
-export default function now(req, res) {
-  const date = new Date();
-  const format = date.toGMTString();
-
-  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-
-  res.json({ now: format });
-}
