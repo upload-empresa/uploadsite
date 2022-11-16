bf608127d1cc75d4b2ace8786dbfac274e9910b6
gabrielGit1 <botelhog275@gmail.com>
2022-11-16 20:13:44 -0300

    Testando a title tags

--- src/components/SEO/head.tsx
+++ src/components/SEO/head.tsx
@@ -9,9 +9,15 @@ interface HeaderProps {
 
 export function Header({ title, description }: HeaderProps) {
     return (
-            <Head>
-                <title>{title} - Sua empresa sempre um passo à frente</title>
-                <meta name='description' content={`${description}`} />
-            </Head>
-        )
+        <Head>
+            <meta charSet="UTF-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
+            <title>{title} - Sua empresa sempre um passo à frente</title>
+            <meta name='description' content={`${description}`} />
+        </Head>
+    )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,20 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
     render() {
         return (
-            <Html lang="pt-br">
-                <Head>
-                    <meta charSet="UTF-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
+                <Html lang='pt-br'>
+                    <Head />
+                    <body>
+                        <Main />
+                        <NextScript />
+                    </body>
+                </Html>
         )
     }
 }
