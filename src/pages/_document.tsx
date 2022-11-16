328dc81ba28acb35037c452fc8721a458e9bbf70
gabrielGit1 <botelhog275@gmail.com>
2022-11-16 19:51:02 -0300

    Testado as meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,16 +6,17 @@ export default class MyDocument extends Document {
             <Html lang="pt-br">
                 <Head>
                     <meta charSet="UTF-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />                
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
                 </Head>
-                        <body>
-                            <Main />
-                            <NextScript />
-                        </body>
-                    </Html>
-                    )
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
     }
 }
