129bf0a659e4874f635a8ac0ff81fc007fc52218
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2023-07-11 09:43:26 -0300

    Consertando erros de state

--- src/pages/admin/posts.js
+++ src/pages/admin/posts.js
@@ -1,33 +1,49 @@
-import axios from "axios";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
-import NextLink from "next/link";
-import React, { useEffect, useContext, useReducer } from "react";
-import { getError } from "../../utils/error";
-import { LoginProfile } from "../../utils/loginProfile";
-import { useSnackbar } from "notistack";
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import NextLink from 'next/link';
+import React, { useEffect, useContext, useReducer } from 'react';
+import {
+  TableContainer,
+  Table,
+  TableCaption,
+  Button,
+  Icon,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  HStack,
+  Text,
+  Heading,
+} from '@chakra-ui/react';
+import { getError } from '../../utils/error';
+import { MdAdd, MdModeEdit, MdOutlineDelete } from 'react-icons/md';
+import { Store } from '../../utils/Store';
+import { useSnackbar } from 'notistack';
 
 function reducer(state, action) {
   switch (action.type) {
-    case "FETCH_REQUEST":
-      return { ...state, loading: true, error: "" };
-    case "FETCH_SUCCESS":
-      return { ...state, loading: false, posts: action.payload, error: "" };
-    case "FETCH_FAIL":
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, posts: action.payload, error: '' };
+    case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
-    case "CREATE_REQUEST":
+    case 'CREATE_REQUEST':
       return { ...state, loadingCreate: true };
-    case "CREATE_SUCCESS":
+    case 'CREATE_SUCCESS':
       return { ...state, loadingCreate: false };
-    case "CREATE_FAIL":
+    case 'CREATE_FAIL':
       return { ...state, loadingCreate: false };
-    case "DELETE_REQUEST":
+    case 'DELETE_REQUEST':
       return { ...state, loadingDelete: true };
-    case "DELETE_SUCCESS":
+    case 'DELETE_SUCCESS':
       return { ...state, loadingDelete: false, successDelete: true };
-    case "DELETE_FAIL":
-      return { ...state, loadingDelete: false, successDelete: false };
-    case "DELETE_RESET":
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false };
+    case 'DELETE_RESET':
       return { ...state, loadingDelete: false, successDelete: false };
     default:
       state;
@@ -35,33 +51,33 @@ function reducer(state, action) {
 }
 
 function AdminPosts() {
-  const { state } = useContext(LoginProfile);
+  const { state } = useContext(Store);
   const router = useRouter();
   const { userInfo } = state;
 
   const [{ posts, successDelete }, dispatch] = useReducer(reducer, {
     loading: true,
     posts: [],
-    error: "",
+    error: '',
   });
 
   useEffect(() => {
     if (!userInfo) {
-      router.push("/login");
+      router.push('/login');
     }
     const fetchData = async () => {
       try {
-        dispatch({ type: "FETCH_REQUEST" });
-        const { data } = await axios.get(`api/admin/posts`, {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/posts`, {
           headers: { authorization: `Bearer ${userInfo.token}` },
         });
-        dispatch({ type: "FETCH_SUCCESS", payload: data });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     if (successDelete) {
-      dispatch({ type: "DELETE_RESET" });
+      dispatch({ type: 'DELETE_RESET' });
     } else {
       fetchData();
     }
@@ -69,46 +85,93 @@ function AdminPosts() {
 
   const { enqueueSnackbar } = useSnackbar();
   const createHandler = async () => {
-    if (!window.confirm("Tem certeza?")) {
+    if (!window.confirm('Tem certeza?')) {
       return;
     }
     try {
-      dispatch({ type: "CREATE_REQUEST" });
-      const { data } = axios.post(
-        `/api/admin/post'`,
+      dispatch({ type: 'CREATE_REQUEST' });
+      const { data } = await axios.post(
+        `/api/admin/posts`,
         {},
         {
           headers: { authorization: `Bearer ${userInfo.token}` },
         }
       );
-      dispatch({ type: "CREATE_SUCCESS" });
-      enqueueSnackbar("Post criado com sucesso!", { variant: "success" });
-      router.push(`/admin/posts/${data.post._id}`);
+      dispatch({ type: 'CREATE_SUCCESS' });
+      enqueueSnackbar('Post criado com sucesso!', { variant: 'success' });
+      router.push(`/admin/post/${data.post._id}`);
     } catch (err) {
-      dispatch({ type: "CREATE_FAIL" });
-      enqueueSnackbar(getError(err), { variant: "error" });
+      dispatch({ type: 'CREATE_FAIL' });
+      enqueueSnackbar(getError(err), { variant: 'error' });
     }
   };
-
   const deleteHandler = async (postId) => {
-    if (!window.confirm("Tem certeza?")) {
+    if (!window.confirm('Tem certeza?')) {
       return;
     }
     try {
-      dispatch({ type: "DELETE_REQUEST" });
+      dispatch({ type: 'DELETE_REQUEST' });
       await axios.delete(`/api/admin/posts/${postId}`, {
         headers: { authorization: `Bearer ${userInfo.token}` },
       });
-      dispatch({ type: "DELETE_SUCCESS" });
-      enqueueSnackbar("Post deletado com sucesso!", { variant: "success" });
+      dispatch({ type: 'DELETE_SUCCESS' });
+      enqueueSnackbar('Post deletado com sucesso!', { variant: 'success' });
     } catch (err) {
-      dispatch({ type: "DELETE_FAIL" });
-      enqueueSnackbar(getError(err), {variant: 'eror'})
+      dispatch({ type: 'DELETE_FAIL' });
+      enqueueSnackbar(getError(err), { variant: 'error' });
     }
   };
-
   return (
-    <></> //FRONT END
+    <>
+      <TableContainer px={24} py={7} mb="3%">
+        <Heading>Todos os Posts</Heading>
+        <Table variant="simple">
+          <TableCaption placement="top" textAlign="right">
+            <Button onClick={createHandler} colorScheme="green">
+              <Icon as={MdAdd} pr={1} fontSize="28px" />
+              <Text fontSize="16px">Adicionar</Text>
+            </Button>
+          </TableCaption>
+          <TableContainer>
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>TITULO</Th>
+                  <Th>AÇÕES</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {posts.map((post) => (
+                  <Tr key={post._id}>
+                    <Td>{post.title}</Td>
+                    <Td>
+                      <NextLink href={`/admin/post/${post._id}`} passHref>
+                        <Button
+                          bg="black"
+                          color="white"
+                          _hover={{ bg: 'black' }}
+                        >
+                          <Icon as={MdModeEdit} fontSize="28px" />
+                        </Button>
+                      </NextLink>{' '}
+                      <Button
+                        bg="red"
+                        color="white"
+                        _hover={{ bg: 'red' }}
+                        onClick={() => deleteHandler(post._id)}
+                      >
+                        <Icon as={MdOutlineDelete} fontSize="28px" />
+                      </Button>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Table>
+      </TableContainer>
+    </>
   );
-
 }
+
+export default dynamic(() => Promise.resolve(AdminPosts), { ssr: false });
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const signToken = (user) => {
   return jwt.sign(
@@ -10,7 +10,7 @@ const signToken = (user) => {
 
     process.env.JWT_SECRET,
     {
-      expiresIn: "30d",
+      expiresIn: '30d',
     }
   );
 };
@@ -21,14 +21,14 @@ const isAuth = async (req, res, next) => {
     const token = authorization.slice(7, authorization.length);
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(401).send({ message: "Token is not valid" });
+        res.status(401).send({ message: 'Token is not valid' });
       } else {
         req.user = decode;
         next();
       }
     });
   } else {
-    res.status(401).send({ message: "Token is not suppiled" });
+    res.status(401).send({ message: 'Token is not suppiled' });
   }
 };
 
